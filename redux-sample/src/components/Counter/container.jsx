3f5167509a8ee6228a82724bc7302cbf8c35968d
工藤祐 <kudouyuu@kudouyuunoMacBook-Pro.local>
2020-07-17 22:52:16 +0900

    カレンダー実装

--- redux-sample/src/components/Counter/container.jsx
+++ redux-sample/src/components/Counter/container.jsx
@@ -1,3 +1,25 @@
+// ==========ここから追加する==========
+import { connect } from "react-redux";
+
+import { increment, decrement } from "../../redux/count/actions";
+// ==========ここまで追加する==========
+
 import Counter from "./presentation";
 
-export default Counter;
+// ==========ここから追加する==========
+const mapStateProps = ({ count }) => ({ count });
+
+const mapDispatchProps = dispatch => ({
+  increment: count => {
+    dispatch(increment(count));
+  },
+  decrement: count => {
+    dispatch(decrement(count));
+  }
+});
+
+export default connect(
+  mapStateProps,
+  mapDispatchProps
+)(Counter);
+// ==========ここまで追加する==========
--- redux-sample/src/index.js
+++ redux-sample/src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// ==========ここから追加する==========
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import rootReducer from "./redux/rootReducer";
 import Counter from "./components/Counter/container";
 
-const App = () => <Counter />;
+const store = createStore(rootReducer);
+// ==========ここまで追加する==========
+
+// ==========ここから編集する==========
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
+// ==========ここまで編集する==========
 
 ReactDOM.render(<App />, document.getElementById("root"));
